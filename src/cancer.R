a459a4081a35af995cfadd480c8bb7aaa04cfbd7
Dave Bennett <episkipoe@gmail.com>
2013-04-14 18:44:13 -0500

    Added cancer.R and evolution.pl

---
+++ src/cancer.R
@@ -0,0 +1,25 @@
+#!/usr/bin/Rscript
+
+A portrait of an onco gene:
+slices of time caught in vitro
+while the patient wastes away in vivo
+I watch a vascular structure grow in silico
+and at the end of the day commiserate in vino.
+
+Technology reveals a tumor's proclivities             '
+The exact ways in which it plans to kill me.
+But its modus operandi includes a bag of tricks,
+Metastasis and mutation, which makes time an enemy
+
+Science is occasionally about statistical rigor.
+And is dominated by images and intuition.
+So I write some code that is tactile.
+I need my fingers in the DNA.
+
+To separate cell signal from the noise.
+The chimeric wheat from the chaff.
+Find the mechanism of action
+And viable drug targets
+And kill it first.
+
+
---
+++ src/evolution.pl
@@ -0,0 +1,10 @@
+#!/bin/perl
+
+Evolution by means of natural selection
+is throwing code against the wall to see what runs
+So be prepared for some insanity when you decompile humanity.
+Yes, that means that when I said that this script looks "evolved"
+it was not intended as a compliment.
+
+
+
