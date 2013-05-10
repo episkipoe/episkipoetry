fecdec81e711a145a45081115ccecae7ba67688f
Dave Bennett <episkipoe@gmail.com>
2013-05-09 20:37:37 -0500

    add drunkScience

---
+++ src/drunkScience.R
@@ -0,0 +1,38 @@
+#!/usr/bin/Rscript
+
+Drunk science
+Is my favorite kind of science.
+Merely a wee bit, enough to get the mind wandering
+Loosen up inhibitions and reduce reliance on conventional thinking
+Everyone feels that they can fix the world when they've been drinking.'
+
+Reading about the role of methylation in bee brains
+while engaging in my own ethanol-based epigenetic modification.
+Leaves me wondering:  if I ate enough royal jelly, would I become a queen, too?
+
+Looking at the hypothesis through beer goggles.
+That which initially seemed inconceivable
+3 sheets of white paper to the wind 
+Now is a forgone conclusion.
+
+A drink later and I enter the belligerent stage,
+Criticizing the author's custom perl scripts.'
+
+Too wobbly to keep my balance on the table
+But I can still differentiate anything differentiable
+
+I may be slurring my speech 
+But I can still scramble up a sigmoidal activation function
+
+Security tells me to get down off the light pole,  
+There are plenty of trees to climb.
+Hanging out with my best bud.
+A minor altercation but now all is forgiven.
+And after he passed out we wrote a draft for PNAS on his face in permanent marker.
+Rum and coke is my coauthor.
+
+The next morning groggy, but eager to gaze upon my magnum opus.
+Instead I find sentences that that reflect an embarrassing level of:
+inanity:  "to live well is to love.  but to love is to lose"
+insanity:  "Steins and truth.  Dimples and passports."
+incompetence:  "life moves for me slow and hazy"
